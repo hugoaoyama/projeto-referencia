4d85e0ba5a79c6e381bc9132a4368ae4c5ef4eb2
hugoaoyama <hugoaoyama@gmail.com>
2019-05-01 15:03:03 -0300

    adicionando o primeiro fonte do projeto

---
+++ src/main/java/com/h2desenvolvimento/projetoReferencia/api/ProjetoReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.h2desenvolvimento.projetoReferencia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReferenciaApplication.class, args);
+	}
+
+}
